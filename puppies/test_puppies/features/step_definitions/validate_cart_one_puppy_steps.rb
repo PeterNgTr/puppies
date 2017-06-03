9017dbc596f0e540722592e2d83c83314db53b2a
germaniteen <germaniteen@gmail.com>
2017-06-03 18:45:06 +0200

    Update validate_cart_one_puppy_steps.rb

--- puppies/test_puppies/features/step_definitions/validate_cart_one_puppy_steps.rb
+++ puppies/test_puppies/features/step_definitions/validate_cart_one_puppy_steps.rb
@@ -1,12 +1,11 @@
 Then(/^I should see "([^"]*)" as the name for line item (\d+)$/) do |name, line_item|
-  @cart.name_for_line_item(line_item.to_i).should include name
+  expect(on(ShoppingCartPage).name_for_line_item(line_item.to_i).include? name).to be true
 end
 
-And(/^I should see "([^"]*)" as the subtotal for line item (\d+)$/) do |subtotal, line_item|
-  @cart.subtotal_for_line_item(line_item.to_i).should == subtotal
+Then(/^I should see "([^"]*)" as the subtotal for line item (\d+)$/) do |subtotal, line_item|
+  expect(on(ShoppingCartPage).subtotal_for_line_item(line_item.to_i).eql? subtotal).to be true
 end
 
-
-And(/^I should see "([^"]*)" as the cart total$/) do |total|
-  @cart.cart_total.should == total
+Then(/^I should see "([^"]*)" as the cart total$/) do |total|
+  expect(on(ShoppingCartPage).cart_total.eql? total).to be true
 end
